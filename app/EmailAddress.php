c0220f69dbe4a2eb16ff17499dfdf454adda78f9
Chris Beaver <chris@chrisbeaver.net>
2017-02-19 20:06:40 -0500

    Unique composite index on email and campaign id.

--- app/EmailAddress.php
+++ app/EmailAddress.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class EmailAddress extends Model
 {
-    //
+    protected $fillable = ['campaign_id', 'email'];
 }
--- database/migrations/2017_02_19_171503_create_email_addresses_table.php
+++ database/migrations/2017_02_19_171503_create_email_addresses_table.php
@@ -17,11 +17,11 @@ class CreateEmailAddressesTable extends Migration
             $table->increments('id');
             $table->integer('campaign_id')->unsigned();
             $table->string('email');
-            $table->boolean('confirmed');
+            $table->boolean('confirmed')->default(FALSE);
             $table->timestamps();
 
             $table->index('campaign_id');
-            $table->unique('email');
+            $table->unique(['email', 'campaign_id']);
         });
     }
 
