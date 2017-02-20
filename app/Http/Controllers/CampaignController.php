08a9defedbda551bba30bbc4b365a29bafbae9fd
Chris Beaver <chris@chrisbeaver.net>
2017-02-19 21:42:32 -0500

    Form POST request saving data with campaign.

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -4,23 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Campaign;
+use App\EmailAddress;
 use App\Http\Requests\SaveEmailRequest;
 
 class CampaignController extends Controller
 {
     public function showLandingPage(Campaign $campaign)
     {
-    	return dd($campaign);
+    	return view($campaign->subdomain.'.index', compact('campaign'));
     }
 
-    public function showConfirmationPage()
+    public function showConfirmationPage(Campaign $campaign, EmailAddress $email)
     {
+    	return dd($email);
+    	// return view($campaign.'.confirmation');
+    }
+
+    public function saveEmailAddress(Campaign $campaign, SaveEmailRequest $request)
+    {
+    	$email = EmailAddress::create(['email' => $request->email,
+    								   'campaign_id' => $campaign->id]);
     	
-    }
-
-    public function saveEmailAddress(SaveEmailRequest $request)
-    {
-    	Campaign::create(['campaign_id' => $request->campaign_id,
-    					  'email' => $request->email]);
+    	return redirect()->action('CampaignController@showConfirmationPage', compact('email'));
     }
 }
